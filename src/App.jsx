65239adab8f2ab52ac1f392b38d390e1eee7652b
reubecool070 <reubenthapa070@gmail.com>
2024-07-20 22:55:51 +0545

    fix: camera position to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import Ground from "./components/Ground";
-import { Vector3 } from "three";
 
 function App() {
   return (
     <Canvas
       shadows
       camera={{
-        position: [14.167974189157714, 4.665839685727069, 16.523877404187285],
-        rotate: new Vector3(
-          -0.0005901883181447743,
-          0.3230309771883191,
-          1.5726555240453586
-        ),
         fov: 30,
         near: 0.1,
         far: 1000,
--- src/components/Ground/index.jsx
+++ src/components/Ground/index.jsx
@@ -1,25 +1,36 @@
 /* eslint-disable react/prop-types */
-import { OrbitControls, Plane, Stats } from "@react-three/drei";
-import { Color } from "three";
-import { useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { CameraControls, Plane, Stats } from "@react-three/drei";
+import * as THREE from "three";
 
 const Ground = () => {
-  const camera = useThree((s) => s.camera);
+  const controlsRef = useRef(); // Ref for OrbitControls
+  const tileRef = useRef(); // Ref for OrbitControls
+  // const set = useThree((state) => state.set);
+  const tiles = [];
+  const rows = 10;
+  const cols = 10;
 
   const Tile = ({ position, color }) => {
     return (
-      <Plane args={[1, 1]} position={position} receiveShadow>
-        <meshStandardMaterial color={color} />
+      <Plane
+        ref={position[0] === 5 && position[1] === 5 ? tileRef : null}
+        args={[1, 1]}
+        position={position}
+        receiveShadow
+      >
+        <meshLambertMaterial color={color} />
       </Plane>
     );
   };
 
-  const tiles = [];
-  const rows = 10;
-  const cols = 10;
   for (let x = 0; x < rows; x++) {
     for (let y = 0; y < cols; y++) {
-      const color = new Color(Math.random(), Math.random(), Math.random());
+      const color = new THREE.Color(
+        Math.random(),
+        Math.random(),
+        Math.random()
+      );
       tiles.push({
         startNode: 0,
         finishNode: 0,
@@ -30,14 +41,27 @@ const Ground = () => {
     }
   }
 
-  const centerX = rows / 2;
-  const centerY = cols / 2;
-  const distance = Math.max(rows, cols);
-  console.log(centerX, centerY, distance);
-  console.log(camera);
-  camera.addEventListener("update", () => {
-    console.log("updating");
-  });
+  // Function to reset the camera
+  const resetCamera = () => {
+    // Ensure controlsRef.current is defined
+    if (!tileRef.current || !controlsRef.current) return;
+
+    controlsRef.current.fitToBox(tileRef.current, false, {
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 6,
+      paddingBottom: 5,
+    });
+    controlsRef.current.truckSpeed = 0;
+  };
+
+  useEffect(() => {
+    if (tileRef.current && controlsRef.current) {
+      resetCamera();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tileRef.current, controlsRef.current]);
+
   return (
     <>
       <ambientLight intensity={0.5} />
@@ -53,11 +77,14 @@ const Ground = () => {
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
       />
-      <OrbitControls
-        enableZoom={true}
-        // enableRotate={false}
-        // minAzimuthAngle={Math.PI / 2}
-        // enablePan={false} // Disable panning
+      <CameraControls
+        ref={controlsRef}
+        makeDefault
+        maxPolarAngle={0}
+        minPolarAngle={0}
+        maxAzimuthAngle={0}
+        minAzimuthAngle={0}
+        maxDistance={50}
       />
       {tiles.map(({ row, col, color }) => (
         <Tile key={`${row}-${col}`} position={[row, col, 0]} color={color} />
